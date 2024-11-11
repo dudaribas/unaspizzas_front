3dceb0dfc6ee995099950ef1f5aa08eeeb4613de
Taima <gustavotaima18@gmail.com>
2024-11-11 13:30:45 -0300

    feat: refatorando tela de login e implementando lógica

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,16 @@ import { PizzaListComponent } from './components/pizza-list/pizza-list.component
 import { PizzaDetailComponent } from './components/pizza-detail/pizza-detail.component';
 import { CartComponent } from './components/cart/cart.component';
 import { UserOrderListComponent } from './components/user-order-list/user-order-list.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   { path: '', component: PizzaListComponent },
   { path: 'pizza/:id', component: PizzaDetailComponent },
   { path: 'cart', component: CartComponent },
   { path: 'my-orders', component: UserOrderListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 export const appConfig: ApplicationConfig = {
